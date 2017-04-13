05d1757c0147c5c0861099ead99976330851a1ef
Tom Ogasawara <tom.ogasawara@teselagen.com>
2017-04-12 17:32:49 -0700

    initial commit

---
+++ css_practice/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CSSPractice from '../Components/CSSPractice.jsx';
+
+ReactDOM.render(
+  <CSSPractice />,
+  document.getElementById('root')
+);
