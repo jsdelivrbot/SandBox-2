5186f3e3f2a5f236d672cd5c4253322e36c0757f
Tom Ogasawara <tom.ogasawara@teselagen.com>
2017-04-19 15:15:34 -0700

    Write BookList container

---
+++ LibraryApp/src/reducers/book_reducer.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    { title: "Essential Javascript"},
+    { title: "Lord of the Rings"},
+    { title: "Harry Potter"},
+    { title: "Batman"}
+  ];
+}
--- LibraryApp/src/reducers/index.js
+++ LibraryApp/src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import BookReducer from './book_reducer';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  books: BookReducer
 });
 
 export default rootReducer;
