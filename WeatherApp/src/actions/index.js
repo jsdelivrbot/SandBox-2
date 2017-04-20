8110047cb1990f9cf6de74be2d937615ac447eb7
Tom Ogasawara <tom.ogasawara@teselagen.com>
2017-04-20 15:48:19 -0700

    Add API call to fetch weather data

---
+++ WeatherApp/src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = '4a756b35bdef4da87f0bf4227ac0a859';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export const fetchWeather = (city) => {
+  const url = `${ROOT_URL}&q=${city},us`;
+
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+};
