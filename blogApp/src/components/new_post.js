44f98f137ab4698548683967ae57d0610fbcdb82
Tom Ogasawara <tom.ogasawara@teselagen.com>
2017-04-27 11:00:09 -0700

    Build component heirarchy

---
+++ blogApp/src/components/new_post.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewPost extends Component {
+  render() {
+    return (
+      <div>
+        New Posts
+      </div>
+    );
+  }
+}
+
+export default NewPost;
--- blogApp/src/index.js
+++ blogApp/src/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/posts_index.js';
+import NewPost from './components/new_post';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/posts/new" component={NewPost} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
