cf7d12fc7a9298db461e4005ca711a94b837504e
Tom Ogasawara <tom.ogasawara@teselagen.com>
2017-05-04 11:01:24 -0700

    Initial commit

--- blogApp/src/reducers/posts_reducer.js
+++ blogApp/src/reducers/posts_reducer.js
@@ -4,13 +4,6 @@ import _ from 'lodash';
 export default function (state = {}, action ) {
   switch (action.type) {
     case FETCH_POST:
-      // ES5:
-      // const post = action.payload.data;
-      // const newState = { ...state,  };
-      // newState[post.id] = post;
-      // return newState;
-
-      //ES6:
       return { ...state, [action.payload.data.id]: action.payload.data };
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
