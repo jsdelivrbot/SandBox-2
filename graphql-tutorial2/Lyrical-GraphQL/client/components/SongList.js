663a667c007d05696a320a86d4062e1b530bcd3a
Tom Ogasawara <tom.ogasawara@teselagen.com>
2017-05-10 10:45:25 -0700

    Fix rendering bug in song list

--- graphql-tutorial2/Lyrical-GraphQL/client/components/SongList.js
+++ graphql-tutorial2/Lyrical-GraphQL/client/components/SongList.js
@@ -1,15 +1,26 @@
-import React, { Component } from 'react';
-import gql from 'graphql-tag';
-import { graphql } from 'react-apollo';
+import React, { Component } from 'react'
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
 
 class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map(song => {
+      return (
+        <li key={song.id} className="collection-item">
+          {song.title}
+        </li>
+      )
+    })
+  }
+
   render() {
-    console.log(this.props);
+    if (this.props.data.loading) return <div>Loading...</div>
 
     return (
       <div>
-        SongList
-
+        <ul className="collection">
+          {this.renderSongs()}
+        </ul>
       </div>
     )
   }
@@ -18,9 +29,10 @@ class SongList extends Component {
 const query = gql`
   {
     songs {
+      id
       title
     }
   }
-`;
+`
 
-export default graphql(query)(SongList);
+export default graphql(query)(SongList)
