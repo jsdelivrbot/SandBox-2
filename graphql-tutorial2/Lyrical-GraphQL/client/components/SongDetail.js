69dc11d37aa5f0e637ac055b4d6df7a97d03ba27
Tom Ogasawara <tom.ogasawara@teselagen.com>
2017-05-11 13:55:33 -0700

    Add song detail component

---
+++ graphql-tutorial2/Lyrical-GraphQL/client/components/SongDetail.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
+import { Link } from 'react-router'
+
+class SongDetail extends Component {
+  render() {
+    return (
+      <div>
+        song detail
+      </div>
+    )
+  }
+}
+
+export default SongDetail
