3d816c84481a85d7d07bf6abdd0a76ec7044db2d
Tom Ogasawara <tom.ogasawara@teselagen.com>
2017-05-15 17:47:09 -0700

    Remove additional unnecessary files

--- css_practice/src/index.js
+++ css_practice/src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+
 import App from './App';
 import './index.css';
 
+const Root = () => {
+  return (
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+        </Route>
+      </Router>
+  )
+}
+
 ReactDOM.render(
-  <App />,
+  <Root />,
   document.getElementById('root')
-);
+)
+
+
+// <IndexRoute component={SongList} />
+// <Route path="songs/new" component={SongCreate} />
+// <Route path="songs/:id" component={SongDetail} />
