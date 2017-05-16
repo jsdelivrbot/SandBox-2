5a3413494ac6c6a7e365bd78cb3349e3c16a5eb5
Tom Ogasawara <tom.ogasawara@teselagen.com>
2017-05-15 17:31:39 -0700

    Remove old code

--- css_practice/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CSSPractice from '../Components/CSSPractice.jsx';
-
-ReactDOM.render(
-  <CSSPractice />,
-  document.getElementById('root')
-);
