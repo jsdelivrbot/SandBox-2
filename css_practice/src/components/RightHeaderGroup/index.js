5a49b8a608a3589d0739a129c531be9ee19b9af0
Tom Ogasawara <tom.ogasawara@teselagen.com>
2017-05-28 10:57:02 -0700

    Add header icons

--- css_practice/src/components/RightHeaderGroup/index.js
+++ css_practice/src/components/RightHeaderGroup/index.js
@@ -9,20 +9,30 @@ const RightHeaderGroup = () => {
       <a
         className="zzz header-button linkedin"
         href="https://www.linkedin.com/in/tom-ogasawara"
-      />
+      >
+        <i className="fa fa-linkedin header-icon" aria-hidden="true" />
+      </a>
       <a
         className="zzz header-button github"
         href="https://www.github.com/tom-ogasawara"
-      />
+      >
+        <i className="fa fa-github header-icon" aria-hidden="true" />
+      </a>
       <a
         className="zzz header-button portfolio"
         href="http://www.tom-ogasawara.com"
-      />
+      >
+        <i className="fa fa-folder-open header-icon" aria-hidden="true" />
+      </a>
       <a
         className="zzz header-button email"
         href="mailto:tom.ogasawara@gmail.com"
-      />
-      <Link className="zzz header-button resume" to="/resume" />
+      >
+        <i className="fa fa-envelope header-icon" aria-hidden="true" />
+      </a>
+      <Link className="zzz header-button resume" to="/resume">
+        <i className="fa fa-file-text header-icon" aria-hidden="true" />
+      </Link>
     </div>
   )
 }
