accfef55b8b685eaed59afb3ac597406269de87e
Tom Ogasawara <tom.ogasawara@teselagen.com>
2017-06-23 19:29:33 -0700

    Update sidebar styling

--- css_practice/src/components/IndexMenu/index.js
+++ css_practice/src/components/IndexMenu/index.js
@@ -1,36 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router'
+import React from 'react';
+import { Link } from 'react-router';
 
-import './style.css'
+import './style.css';
 
 class IndexMenu extends React.Component {
+  renderLinks(links) {
+    return links.map((link, i) => {
+      return (
+        <div className="zzz index-tab-container">
+          <Link to={`/sections/${link.id}`} className="index-tab">
+            <div className="index-tab-number">{link.id}</div>
+            <div className="index-tab-title">
+              {link.title}
+            </div>
+          </Link>
+        </div>
+      );
+    });
+  }
+
   render() {
+    const links = [
+      { id: 1, title: 'Section' },
+      { id: 2, title: 'Section' },
+      { id: 3, title: 'Section' },
+      { id: 4, title: 'Section' },
+      { id: 5, title: 'Section' },
+      { id: 6, title: 'Section' }
+    ];
+
     return (
       <div className="zzz app-container">
         <div className="zzz index-title">
           Index
         </div>
-        <div className="zzz index-tab">
-          <Link to="/sections/1">Section One</Link>
-        </div>
-        <div className="zzz index-tab">
-          <Link to="/sections/2">Section Two</Link>
-        </div>
-        <div className="zzz index-tab">
-          <Link to="/sections/3">Section Three</Link>
-        </div>
-        <div className="zzz index-tab">
-          <Link to="/sections/4">Section Four</Link>
-        </div>
-        <div className="zzz index-tab">
-          <Link to="/sections/5">Section Five</Link>
-        </div>
-        <div className="zzz index-tab">
-          <Link to="/sections/6">Section Six</Link>
-        </div>
+        {this.renderLinks(links)}
       </div>
-    )
+    );
   }
 }
 
-export default IndexMenu
+export default IndexMenu;
