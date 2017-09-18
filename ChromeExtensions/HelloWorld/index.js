0d341a2cab38f0b1533c3e661506b10e132cf7a9
Tom Ogasawara <tom.ogasawara@teselagen.com>
2017-09-17 21:59:49 -0700

    update index component

--- ChromeExtensions/HelloWorld/index.js
+++ ChromeExtensions/HelloWorld/index.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import './style.css'
 
 class Extensions extends Component {
   render() {
     return (
-      <div>
-        Extensions
+      <div className="container">
+        <div className="header">
+          <div className="home-buttom">Home</div>
+          Header
+        </div>
+        <div className="content">
+          Extensions
+        </div>
       </div>
     );
   }
