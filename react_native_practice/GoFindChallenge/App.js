f7aaa56dba49711f7030d79d191029ad2a305c57
Tom Ogasawara <tom.ogasawara@teselagen.com>
2017-10-10 17:48:27 -0700

    Start new app

---
+++ react_native_practice/GoFindChallenge/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import Header from './components/Header';
+import SearchBar from './components/SearchBar';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header></Header>
+        <SearchBar></SearchBar>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    // justifyContent: 'center',
+    backgroundColor: '#ff7273'
+  },
+  // welcome: {
+  //   fontSize: 20,
+  //   textAlign: 'center',
+  //   margin: 10,
+  // },
+  // instructions: {
+  //   textAlign: 'center',
+  //   color: '#333333',
+  //   marginBottom: 5,
+  // },
+});
