ad4938de85a8104e4ad81549b4662d630e2a9884
Tom Ogasawara <tom.ogasawara@teselagen.com>
2017-10-10 17:57:05 -0700

    Add gofind logo

--- react_native_practice/GoFindChallenge/components/Header.js
+++ react_native_practice/GoFindChallenge/components/Header.js
@@ -1,29 +1,35 @@
-import React, { Component } from 'react'
-import { Platform, StyleSheet, Text, View } from 'react-native'
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, Image } from 'react-native';
 
 export default class Header extends Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          style={styles.logo}
+          source={require('../assets/gofindlogo.png')}
+        />
         <Text style={styles.headerText}>GoFind</Text>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  header: {
     width: '100%',
-    height: 60,
     justifyContent: 'center',
     alignItems: 'center',
-    // backgroundColor: '#5f6360',
-    // backgroundColor: 'rgb(255, 114, 115)',
     marginTop: 20
   },
   headerText: {
     color: '#fff',
     fontSize: 50,
     fontWeight: 'bold'
+  },
+  logo: {
+    height: 80,
+    width: 80,
+    marginTop: 50
   }
-})
+});
